19178aee559d89ed502f630a79bb75fc80baafaf
Artem Muradov <tym32167@gmail.com>
2015-01-08 07:31:02 +0500

    initial

---
+++ src/TaskStart/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System.IO;
+using System.Windows.Input;
+using TaskStart.ViewModel;
+using Application = System.Windows.Application;
+using DataObject = System.Windows.DataObject;
+using DragEventArgs = System.Windows.DragEventArgs;
+
+namespace TaskStart
+{
+	public partial class MainWindow
+	{
+		private readonly MainViewModel _model;
+		public MainWindow()
+		{
+			InitializeComponent();
+
+			Closing += MainWindowClosing;
+			var locator = new ViewModelLocator();
+			_model = locator.Main;
+			DataContext = _model;
+		}
+
+		void MainWindowClosing(object sender, System.ComponentModel.CancelEventArgs e)
+		{
+			Application.Current.Shutdown();
+		}
+
+		
+		private void CloseCommandHandler(object sender, ExecutedRoutedEventArgs e)
+		{
+			Application.Current.Shutdown();
+		}
+
+		
+		private void CanExecuteHandler(object sender, CanExecuteRoutedEventArgs e)
+		{
+			e.CanExecute = true;
+		}
+
+
+		private void WindowDrop(object sender, DragEventArgs e)
+		{
+			if (e.Data is DataObject && ((DataObject)e.Data).ContainsFileDropList())
+			{
+				foreach (string filePath in ((DataObject)e.Data).GetFileDropList())
+				{
+					if (File.Exists(filePath))
+						_model.Add(filePath);
+				}
+			}
+		}
+	}
+}
---
+++ src/TaskStart/ViewModel/ViewModelLocator.cs
@@ -0,0 +1,23 @@
+namespace TaskStart.ViewModel
+{
+    public class ViewModelLocator
+    {
+        private static MainViewModel _main;
+       
+        public ViewModelLocator()
+        {
+            _main = new MainViewModel();
+        }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Performance",
+            "CA1822:MarkMembersAsStatic",
+            Justification = "This non-static member is needed for data binding purposes.")]
+        public MainViewModel Main
+        {
+            get
+            {
+                return _main;
+            }
+        }
+    }
+}
